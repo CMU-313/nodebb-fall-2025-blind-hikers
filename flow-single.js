6cb81cc28f41c0fe3c627bd1d07a6ec389384984
juliaschraml <schramljk7@gmail.com>
2025-10-23 21:07:56 +0000

    Fixed linting issues with flow-single.js

--- flow-single.js
+++ flow-single.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
@@ -7,8 +9,8 @@ const { execSync } = require('child_process');
 const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error('Usage: node flow-single.js <file-path>');
-  process.exit(1);
+	console.error('Usage: node flow-single.js <file-path>');
+	process.exit(1);
 }
 
 // Create a temporary .flowconfig that only includes the specific file
@@ -31,17 +33,17 @@ all=true
 fs.writeFileSync('.flowconfig.temp', tempFlowConfig);
 
 try {
-  // Run flow with the temporary config
-  execSync('./node_modules/.bin/flow check --flowconfig-name .flowconfig.temp', { 
-    stdio: 'inherit',
-    cwd: process.cwd()
-  });
+	// Run flow with the temporary config
+	execSync('./node_modules/.bin/flow check --flowconfig-name .flowconfig.temp', {
+		stdio: 'inherit',
+		cwd: process.cwd(),
+	});
 } catch (error) {
-  // Flow errors are expected, just exit with the error code
-  process.exit(error.status || 1);
+	// Flow errors are expected, just exit with the error code
+	process.exit(error.status || 1);
 } finally {
-  // Clean up temporary config
-  if (fs.existsSync('.flowconfig.temp')) {
-    fs.unlinkSync('.flowconfig.temp');
-  }
+	// Clean up temporary config
+	if (fs.existsSync('.flowconfig.temp')) {
+		fs.unlinkSync('.flowconfig.temp');
+	}
 }
